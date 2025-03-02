877e2590c4961927a167bfdc459d0aab29326c84
naimhossain18 <hmnaim.mcet@gmail.com>
2025-03-02 22:47:43 +0600

    added date and time

--- dev.js
+++ dev.js
@@ -1,5 +1,5 @@
 const completedBtn =document.querySelectorAll(".completed-btn");
-console.log(completedBtn);
+// console.log(completedBtn);
 
 for (let i=0; i<completedBtn.length; i++){
     const compBtn = completedBtn[i];
@@ -8,6 +8,9 @@ for (let i=0; i<completedBtn.length; i++){
         alert("Board uploaded Successfully")
 
         event.target.setAttribute('disabled', '')
+
+        const btnParent =(compBtn.parentElement.parentElement.parentElement);
+        const cardTitle = (btnParent.getElementsByClassName("card-title")[0].innerText);
 
         
         const taskNumber =document.getElementById("task-number");
@@ -20,9 +23,10 @@ for (let i=0; i<completedBtn.length; i++){
         if( parseInt(taskNumber.innerText)=== 0) {
             alert("congratulation!!! You have completed all the current task");
         }
-
+        let now = new Date();
         const completedNotification = document.createElement("p");
-        completedNotification.innerText = "you have completed the task Fix Mobile Button Issue at 1:55:12 AM";
+        completedNotification.className = "my-2 bg-[#F4F7FF] p-2 rounded-lg"
+        completedNotification.innerText =`you have completed the task ${cardTitle} at ${now.toLocaleTimeString()}`;
         document.getElementById("notification");
         notification.appendChild(completedNotification);
 
@@ -41,3 +45,23 @@ const clearHistoryBtn = document.getElementById("clear-btn").addEventListener("c
 document.getElementById("discover-btn").addEventListener("click", function(){
     window.location.href= "./index2.html";
 })
+// 
+const topDate = document.getElementById("top-date");
+const topDay = document.getElementById("top-day");
+const dateString = new Date().toDateString();
+const dateStringArray = dateString.split(" ")
+const formatDay = dateStringArray[0];
+dateStringArray.shift();
+const formatDate = dateStringArray.join(" ")
+topDate.innerText= formatDate;
+topDay.innerText = formatDay;
+
+
+// theme-btn
+// const colors = ["red", "green", "blue"];
+// let inx = 0;
+// document.getElementById("theme-btn").addEventListener("click", function(){
+//     document.body.style.backgroundColor = colors[inx]
+//     inx++;
+//     if(inx === colors.length) inx = 0;
+// })
---
+++ utility.js
@@ -0,0 +1,13 @@
+
+
+function colors (){
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`
+    
+}
+function changeColor (){
+    document.body.style.backgroundColor = colors();
+}
